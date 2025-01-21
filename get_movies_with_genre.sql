05f7b01473e0883be74d9131d53493626d5f21b9
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-21 18:35:47 +0800

    add genre sql

---
+++ get_movies_with_genre.sql
@@ -0,0 +1,7 @@
+SELECT movies.title, GROUP_CONCAT(genres.name) as genre FROM movies
+JOIN movie_genres ON movies.id = movie_genres.movie_id
+JOIN genres ON movie_genres.genre_id = genres.id
+WHERE genres.name = "Drama"
+
+
+GROUP BY movies.id
